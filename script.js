52d527c52664d6b44e9cd985177e177c558f469d
brettl1991 <brettl1991@gmail.com>
2022-11-09 15:25:27 +0000

    Fetch data from api

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let apiQuotes = [];
+
+//Show New Quote
+function newQuote() {
+  //Pick a random uote from an apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+//Get Quotes from API
+async function getQuotes() {
+  const apiURL = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiURL);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //Catch Error Here
+  }
+}
+
+//On Load
+getQuotes();
